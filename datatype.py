867a38c3294010cc69f9c7a6d0f627b7cc4258f0
Tarun Kumar <tarunkumar272714@gmail.com>
2024-10-10 08:45:53 +0530

    1st commit

---
+++ datatype.py
@@ -0,0 +1,31 @@
+#Data types ---  int,float,complex,bool,str,list,tuple,range,dictionary,set 
+
+x=5
+print(type(x))
+
+y=2.5
+print(type(y))
+
+z=1.5j
+print(type(z))
+
+x = True
+print(type(x))
+
+x = {"apple", "banana", "cherry"}
+print(type(x))
+
+x = ["apple", "banana", "cherry"]
+print(type(x))
+
+x = ("apple", "banana", "cherry")
+print(type(x))
+
+x = str("Tarun")
+print(type(x))
+
+x = dict(name="Tarun", age=22)
+print(type(x))
+
+x = range(15)
+print(type(x))
